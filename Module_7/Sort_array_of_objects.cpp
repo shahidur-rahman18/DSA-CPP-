38d8be35ba8e51960db24c1de8739cee30c8df38
shahidur-rahman18 <mdshahidurrahman3690@gmail.com>
2026-06-12 11:50:36 +0600

    update 2nd time

--- Module_7/Sort_array_of_objects.cpp
+++ Module_7/Sort_array_of_objects.cpp
@@ -10,15 +10,53 @@ public:
 };
 bool cmp(Student l, Student r)
 {
-    if (l.marks < r.marks)
-    {
-        return true;
-    }
+    // if (l.marks < r.marks)
+    // {
+    //     return true;
+    // }
 
-    else
-    {
-        return false;
-    }
+    // else
+    // {
+    //     return false;
+    // }
+
+    // return l.marks > r.marks;  // for shortcut
+    // return l.marks >= r.marks;  // for shortcut when marks are same
+
+    
+    
+    // when marks are qual then sort with roll descending order 
+
+
+    // if (l.marks > r.marks)
+    // {
+    //     return true;
+    // }
+    // else if(l.marks < r.marks){
+    //     return false;
+    // }
+
+    // else
+    // {
+    //     if(l.roll< r.roll)
+    //     {
+    //         return true;
+    //     }
+    //     else
+    //     {
+    //         return false;
+    //     }
+    // }
+    
+   // when marks are qual then sort with roll descending order  --> shortcut
+   if(l.marks==r.marks)
+   {
+    return l.roll < r.roll;
+   }
+   else{
+    return l.marks > r.marks;
+   }
+
 }
 
 int main()
