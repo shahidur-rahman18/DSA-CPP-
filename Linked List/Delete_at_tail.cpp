43d4bbfa050b84d93528fd28ce343bb78c6c7c89
shahidur-rahman18 <mdshahidurrahman3690@gmail.com>
2026-07-11 18:49:55 +0600

    add linked list insert,delete,reverse oreder date: 11-07-2026

---
+++ Linked List/Delete_at_tail.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+
+public:
+    int val;
+    Node *next;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+void insert_at_tail(Node *&head, Node *&tail, int val)
+{
+    Node *newnode = new Node(val);
+    if (head == NULL)
+    {
+        head = newnode;
+        tail = newnode;
+        return;
+    }
+    tail->next = newnode;
+    tail = newnode;
+}
+
+void print_linked_list(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->val << endl;
+        temp = temp->next;
+    }
+}
+
+void delete_at_any_position(Node *&head, Node *&tail, int indx)
+{
+    Node *temp = head;
+    for (int i = 1; i < indx; i++)
+    {
+        temp = temp->next;
+    }
+
+    Node *deleteNode = temp->next;
+    temp->next = temp->next->next;
+    delete deleteNode;
+    tail = temp;
+}
+
+int main()
+{
+    Node *head = NULL;
+    Node *tail = NULL;
+    int val;
+    while (true)
+    {
+        cin >> val;
+        if (val == -1)
+        {
+            break;
+        }
+        insert_at_tail(head, tail, val);
+    }
+      cout << "before delete " << tail->val << endl;
+    delete_at_any_position(head, tail, 3);
+
+  
+    print_linked_list(head);
+    cout << "After delete " << tail->val << endl;
+
+    return 0;
+}
